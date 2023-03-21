d44b0df65e20e8cc5b4a7607e0b6ae48a049522b
Masters <jordan.masters@accenture.com>
2023-03-21 12:14:46 +0100

    Test love love

--- test.js
+++ test.js
@@ -1,11 +1,15 @@
 
-const chai = require('chai')
+const chai = require('chai');
 const expect = chai.expect;
-describe('Array', function () {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      const foo = 'string'
-      expect(foo).to.be.a('string');
+function donnerScore(points){
+  if(points === '0-0') {
+  return 'love-love';
+  }
+};
+describe('Array',  () => {
+    it('should return love love at the begining', () => {
+      const points = '0-0';
+      const score = donnerScore(points);
+      expect(score).to.equal('love-love');
     });
-  });
 });
