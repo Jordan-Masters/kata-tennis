fb736ce18989fbd72ce9d34c19bb78188a696838
Philippe <m.philippe@accenture.com>
2023-03-21 12:37:03 +0100

    1 point marqué

--- test.js
+++ test.js
@@ -1,15 +1,36 @@
 
 const chai = require('chai');
 const expect = chai.expect;
-function donnerScore(points){
-  if(points === '0-0') {
-  return 'love-love';
-  }
+function traduireTennis(point){
+  let pointTennis = ''
+  switch (point) {
+    case '0': 
+    pointTennis = 'love'; 
+    break;
+    case '1': 
+    pointTennis = '15'; 
+    break;}
+return pointTennis
+}
+function donnerScore(pointFirst, pointSecond){
+  let pointTennisFirst = traduireTennis(pointFirst)
+  let pointTennisSecond = traduireTennis(pointSecond)
+
+  return pointTennisFirst + '-' + pointTennisSecond
 };
-describe('Array',  () => {
-    it('should return love love at the begining', () => {
-      const points = '0-0';
-      const score = donnerScore(points);
+describe('Donner score',  () => {
+    it('should return love-love at the begining', () => {
+      const score = donnerScore('0','0');
       expect(score).to.equal('love-love');
+    });
+
+    it('should return 15-love when first wins point', () => {
+      const score = donnerScore('1','0');
+      expect(score).to.equal('15-love');
+    });
+
+    it('should return love-15 when second wins point', () => {
+      const score = donnerScore('0','1');
+      expect(score).to.equal('love-15');
     });
 });
